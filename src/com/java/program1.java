70482573d19d43b5d1ec376bde673af6c7c0b29a
Darshan P <122440089+Darshan17-Abd@users.noreply.github.com>
2025-05-06 09:29:14 +0530

    Update program1.java

---
+++ src/com/java/program1.java
@@ -0,0 +1,11 @@
+package com.java;
+import java.util.Scanner;
+public class program1{
+	public static void main (String[]args) {
+		System.out.println("Hello world");
+    Scanner sc = new Scanner(System.in);
+     System.out.println("Enter the number:");
+    int n = sc.nextInt();
+    System.out.println("The square of"+n+"is:"(n*n) );
+	}
+}
